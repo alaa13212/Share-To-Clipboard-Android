8f11f9adc8ad538aa7e6114b211c37894010d45a
Ali Albarrak <alaa13212@gmail.com>
2021-03-28 21:46:09 +0300

    Fix issue where link extractor return null-ish entries

--- app/src/main/java/me/barrak/sharetoclipboard/services/extract/UriTextExtractor.kt
+++ app/src/main/java/me/barrak/sharetoclipboard/services/extract/UriTextExtractor.kt
@@ -4,10 +4,11 @@ import android.net.*
 import android.util.*
 import me.barrak.sharetoclipboard.*
 
+private val extractRegex: Regex = Patterns.WEB_URL.toRegex()
+private val hasScheme: Regex = "^(http|https|rtsp)://".toRegex()
 
 class UriTextExtractor : ITextExtractor {
     override fun extractElements(text: String): List<TextElement> {
-        val extractRegex: Regex = Patterns.WEB_URL.toRegex()
         return extractRegex.findAll(text)
             .map { matchResult -> clean(matchResult.value) }
             .toList()
@@ -15,7 +16,8 @@ class UriTextExtractor : ITextExtractor {
 
     private fun clean(text: String): TextElement {
         val elements = mutableListOf<String>().apply {
-            val uri = Uri.parse(text)
+            val uriText = if(hasScheme.containsMatchIn(text)) text else "https://$text"
+            val uri = Uri.parse(uriText)
             add(text.removeSuffix("/")) // Original
             add(uri.scheme + "://" + uri.authority + uri.path) // No params
             add(uri.scheme + "://" + uri.authority) // No path
